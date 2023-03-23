5b709c03a0e837f5231ca21b484db8644760ef54
vivek <vivek@creatingwinners.com>
2023-03-23 13:34:51 +0800

    LargestNumberArr

--- src/com/vivekjava/arrays/LargestNumberArr.java
+++ src/com/vivekjava/arrays/LargestNumberArr.java
@@ -4,11 +4,16 @@ public class LargestNumberArr {
     public static void main(String[] args) {
         int[] num = {1,2,6,10,5,6,8};
         int largest = Integer.MIN_VALUE;
+        int smallest= Integer.MAX_VALUE;
         for (int i = 0; i < num.length; i++) {
             if(num[i]>largest){
                 largest = num[i];
             }
+            if(num[i]<smallest){
+                smallest = num[i];
+            }
+
         }
-        System.out.println(largest);
+        System.out.println(largest+"&&"+smallest);
     }
 }
