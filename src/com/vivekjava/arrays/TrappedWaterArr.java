08633a065eaece41465dce8349349da3f0ee520d
vivek <vivek@creatingwinners.com>
2023-03-25 13:09:46 +0800

    trappedwater

---
+++ src/com/vivekjava/arrays/TrappedWaterArr.java
@@ -0,0 +1,37 @@
+package com.vivekjava.arrays;
+
+public class TrappedWaterArr {
+
+
+    public static int trappedWater(int[] height) {
+        int n = height.length;
+        //calc left max boundary arr
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        //calc right max boundary arr
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+
+        //water level loop
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {4, 2, 0, 6, 3, 2, 5};
+        System.out.println(trappedWater(height));
+
+    }
+}
