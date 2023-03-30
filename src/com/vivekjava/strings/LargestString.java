11517975bcf9decce53ecc9f9a2e079a7eb4e8db
vivek <vivek@creatingwinners.com>
2023-03-31 00:34:06 +0800

    LargestString

---
+++ src/com/vivekjava/strings/LargestString.java
@@ -0,0 +1,16 @@
+package com.vivekjava.strings;
+
+public class LargestString {
+
+
+    public static void main(String[] args) {
+        String[] fruits = {"apple","banana","mango"};
+        String largest = fruits[0];
+        for (int i = 0; i < fruits.length ; i++) {
+            if(largest.compareTo(fruits[i]) < 0){
+                largest= fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
