688fa8a308836eae57b8a12b1696469d68db9ce1
vivek <vivek@creatingwinners.com>
2023-03-31 14:05:27 +0800

    recursion increase/decrease order

---
+++ src/com/vivekjava/recursion/FactorialRecursion.java
@@ -0,0 +1,18 @@
+package com.vivekjava.recursion;
+
+public class FactorialRecursion {
+
+
+
+
+
+    public static void fn(int n){
+
+    }
+
+
+    public static void main(String[] args) {
+       fn(5);
+    }
+
+}
