e90bb9b929b349cf3ef4a03899082f24c34c09c2
vivek <vivek@creatingwinners.com>
2023-04-08 03:44:09 +0800

    print digits

---
+++ src/com/vivekjava/recursion/ShowNumberToString.java
@@ -0,0 +1,21 @@
+package com.vivekjava.recursion;
+
+public class ShowNumberToString {
+
+    static String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    public static void printDigits(int number) {
+        if (number == 0) {
+            return;
+        }
+
+        int lastDigit = number % 10;
+        printDigits(number / 10);
+        System.out.print(digits[lastDigit] + " ");
+    }
+
+    public static void main(String[] args) {
+        printDigits(2019);
+    }
+
+}
