b5c559161b776db290e3d959941eeec6166b8b81
vivek <vivek@creatingwinners.com>
2023-04-22 20:48:18 +0800

    backtrack simple array

---
+++ src/com/vivekjava/backtracking/BackTrackSimpleArray.java
@@ -0,0 +1,28 @@
+package com.vivekjava.backtracking;
+
+public class BackTrackSimpleArray {
+
+    public static void changeArr(int[] arr,int i , int val){
+        //base
+        if (i==arr.length){
+            printArr(arr);
+            return;
+        }
+        arr[i]= val;
+        changeArr(arr,i+1,val+1);
+        arr[i] = arr[i] - 2;//backtracking
+    }
+
+    public static void printArr(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" , ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        changeArr(arr,0,1);
+        printArr(arr);
+    }
+}
