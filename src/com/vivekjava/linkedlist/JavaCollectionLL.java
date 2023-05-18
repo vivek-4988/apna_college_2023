80b9b288c7f9d35551cc716181c84f463931d45a
vivek <vivek@creatingwinners.com>
2023-05-18 23:06:47 +0800

    java collection LinkedinList

---
+++ src/com/vivekjava/linkedlist/JavaCollectionLL.java
@@ -0,0 +1,20 @@
+package com.vivekjava.linkedlist;
+
+import java.util.LinkedList;
+
+public class JavaCollectionLL {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> ll= new LinkedList<>();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        System.out.println(ll);
+
+        ll.removeFirst();
+        ll.removeLast();
+
+        System.out.println(ll);
+
+    }
+}
